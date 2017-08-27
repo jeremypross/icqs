0f1e66003af4e2fe78dd4303905e35274886cb12
Jeremy Ross <jeremypross@gmail.com>
2017-08-27 14:32:11 -0400

    qs 2 & 3

---
+++ q2_array_products/main.js
@@ -0,0 +1,86 @@
+// Question 2: Find product of all integers in an array execpt at index
+// You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+// Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers and returns an array of the products.
+
+  // For example, given:
+    [1, 7, 3, 4]
+
+  // your function would return:
+    [84, 12, 28, 21]
+
+  // by calculating:
+    [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+  // Do not use division in your solution.
+
+// ---------------------------------------------------------------------------
+
+// input array:
+
+let inputArray = [3, 1, 2, 5, 6, 4];
+
+// multiplication of all integers before each index
+// we give index 0 a value of 1 since it has no integers before it
+
+[1, 3, 3 * 1, 3 * 1 * 2, 3 * 1 * 2 * 5, 3 * 1 * 2 * 5 * 6]
+// final array of products before each index
+
+let intArray = [1, 3, 3, 6, 30, 180];
+
+// to get products BEFORE each index
+
+let productsOfAllIntsBeforeIndex = [];
+// for each integer find products of all integers
+// before it - storing todal product so far each time
+let productSoFar = 1;
+
+for (let i=0; i < intArray.length; i++) {
+  productsOfAllIntsBeforeIndex[i] = productSoFar;
+  productSoFar *= intArray[i];
+}
+
+//  walk through array backwards - reverse indices
+  // product AFTER each index
+
+let productsOfAllIntsAfterIndex = [];
+
+let productSoFar = 1;
+
+for (let i = intArray.length; i >= 0; i--) {
+  productsOfAllIntsAfterIndex[i] = productSoFar;
+  productSoFar *= intArray[i];
+}
+
+// to find the products of all ints except int @ index - we go throgh array GREEDILY, twice
+  // 1st - get product of all ints BEFORE index
+  // 2nd - go backwards to get products of all ints AFTER index
+  // 3rd - multiply both products.
+
+// FINAL ANSWER -------------------------------------------------------------------
+
+const getProductsOfAllIntsExceptAtIndex = (intArray) => {
+  // check for at least two prices
+  if (intArray.length < 2) {
+    throw new Error('Need at least two prices!');
+  }
+  // create empty array
+  let productsOfAllIntsExceptIndex = [];
+
+  // for each integer we find the products of all integers BEFORE it
+  // storing total product so far each time
+  let productSoFar = 1;
+  for (let i=0; i<intArray.length; i++) {
+    productOfAllIntsExecptIndex[i] = productSoFar;
+    productSoFar = intArray[i];
+  }
+
+  // for each integer - find product of all integers AFTER
+  // since each index in products has product of all before integers
+  // now we store total product of all other ints
+  let productSoFar = 1;
+  for (let j=intArray.length-1; j>=0; j--) {
+    productOfAllIntsExceptIndex[j] *= productSoFar;
+    productSoFar *= intArray[j];
+  }
+  return productOfAllIntsExceptIndex;
+}
---
+++ q3_highest_product_of_3/main.js
@@ -0,0 +1,49 @@
+// Q3: Highest product of 3 integers from a given array
+  // Given an array of integers, find the highest product you can get from three of the integers
+  // array will always have at least 3 integers
+
+// five variables needed to calculate answer:
+        // - highestProductOf3
+        // - highestProductOf2
+        // - lowestProductOf2
+        // - highest
+        // - lowest
+
+const highestProductOf3 = (arrayOfInts) => {
+  // check for at least 3 array values
+  if (arrayOfInts.length < 3) {
+    throw new Error ('Need at least three integers!')
+  }
+
+  // start at third item (index 2) - pre-populate highest and lowest based on first two items
+  let highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+  let lowest = Math.min(arrayOfInts[0, arrayOfInts[1]]);
+  let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  let lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+
+  // except this one - prepopulated with first 3 items
+  let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+  for (let i=2; i < arrayOfInts.length; i++) {
+    let current = arrayOfInts[i];
+
+    // do we have new highest product of 3?
+    highestProductOf3 = Math.max(
+      highestProductOf3,
+      current * highestProductOf2,
+      current * lowestProductOf2
+    );
+
+    // do we have new highest product of 2?
+    lowestProductOf2 = Math.max(
+      lowestProductOf2,
+      current * highest,
+      current * lowest
+    );
+
+    // do we have new highest? or lowest?
+    highest = Math.max(highest, current);
+    lowest = Math.min(lowest, current);
+  }
+  return highestProductOf3
+}
